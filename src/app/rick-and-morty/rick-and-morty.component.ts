f93d2eba2064bed3b808e248a3a57f6064989686
Edz <kevin.sanchezv@cun.edu.co>
2024-09-26 18:39:01 -0500

    Comit api

--- src/app/rick-and-morty/rick-and-morty.component.ts
+++ src/app/rick-and-morty/rick-and-morty.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RickAndMortyServiceService } from '../service/rick-and-morty-service.service';
-import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
+
 @Component({
   selector: 'app-rick-and-morty',
   templateUrl: './rick-and-morty.component.html',
@@ -9,16 +11,71 @@ import { Router } from '@angular/router';
 export class RickAndMortyComponent implements OnInit {
 
   characters: any[] = [];
+  filteredCharacters: any[] = [];
+  searchTerm: string = '';
+  selectedCharacter: any = null;
+  selectedEpisodes: any[] = [];
 
-  constructor(private rickAndMortyService: RickAndMortyServiceService,private router: Router) { }
+  constructor(
+    private rickAndMortyService: RickAndMortyServiceService,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
     this.rickAndMortyService.getCharacters().subscribe((data: any) => {
       this.characters = data.results;
+      this.filteredCharacters = this.characters; // Inicialmente, todos los personajes están filtrados
     });
   }
-  onCharacterClick(id: number): void {
-    this.router.navigate(['/character', id]);
+
+  get groupedCharacters() {
+    const groups = [];
+    for (let i = 0; i < this.filteredCharacters.length; i += 3) {
+      groups.push(this.filteredCharacters.slice(i, i + 3));
+    }
+    return groups;
   }
 
+  filterCharacters(): void {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredCharacters = this.characters.filter(character => 
+      character.name.toLowerCase().includes(term)
+    );
+  }
+
+  onCharacterClick(id: number): void {
+    this.http.get(`https://rickandmortyapi.com/api/character/${id}`).subscribe((character: any) => {
+      this.selectedCharacter = character;
+      this.selectedEpisodes = character.episode;
+      this.fetchEpisodes(this.selectedEpisodes);
+    });
+  }
+
+  fetchEpisodes(episodeUrls: string[]) {
+    const validUrls = episodeUrls.filter(url => url);
+
+    const episodeRequests = validUrls.map(url => this.http.get(url));
+
+    if (episodeRequests.length > 0) {
+      forkJoin(episodeRequests).subscribe({
+        next: (episodes: any) => {
+          this.selectedEpisodes = episodes.map((episode: any) => ({
+            name: episode.name,
+            season: episode.episode.split('E')[0].replace('S', ''),
+            number: episode.episode.split('E')[1]
+          }));
+        },
+        error: (err) => {
+          console.error('Error al obtener episodios:', err);
+        }
+      });
+    } else {
+      this.selectedEpisodes = [];
+    }
+  }
+
+  goBack(): void {
+    this.selectedCharacter = null;
+    this.selectedEpisodes = [];
+  }
 }
--- src/app/service/rick-and-morty-service.service.ts
+++ src/app/service/rick-and-morty-service.service.ts
@@ -1,6 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+export interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+export interface Episode {
+  id: number;
+  name: string;
+  air_date: string;
+  episode: string;
+  characters: string[];
+  url: string;
+  created: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +43,30 @@ export class RickAndMortyServiceService {
 
   constructor(private http: HttpClient) { }
 
-  getCharacters(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/character`);
+  getCharacters(): Observable<Character[]> {
+    return this.http.get<Character[]>(`${this.apiUrl}/character`).pipe(
+      catchError(error => {
+        console.error('Error fetching characters', error);
+        return throwError(error);
+      })
+    );
   }
 
-  getCharacterById(id: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/character/${id}`);
-  }
-  getCharacterEpisodes(id: number): Observable<any> {
-    return this.http.get<any>(`https://rickandmortyapi.com/api/character/${id}/episode`);
+  getCharacterById(id: number): Observable<Character> {
+    return this.http.get<Character>(`${this.apiUrl}/character/${id}`).pipe(
+      catchError(error => {
+        console.error(`Error fetching character with id ${id}`, error);
+        return throwError(error);
+      })
+    );
   }
 
+  getCharacterEpisodes(id: number): Observable<Episode[]> {
+    return this.http.get<Episode[]>(`${this.apiUrl}/character/${id}/episode`).pipe(
+      catchError(error => {
+        console.error(`Error fetching episodes for character with id ${id}`, error);
+        return throwError(error);
+      })
+    );
+  }
 }
